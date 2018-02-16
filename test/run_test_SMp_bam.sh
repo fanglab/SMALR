dda17782c421b5e88a820c71c2af11bd1820783e
John Beaulaurier <john.beaulaurier@gmail.com>
2018-02-16 15:42:07 -0800

    adding some small data for BAM testing

---
+++ test/run_test_SMp_bam.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+smalr -i --SMp --motif=CATG --mod_pos=2 --procs=4 -c 1 --wgaCovThresh=1 input_SMp_bam.txt 
---
+++ test/run_test_SMsn_bam.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+smalr -i --SMsn --motif=CATG --mod_pos=2 --procs=4 --useZMW -c 1 --wgaCovThresh=1 input_SMsn_bam.txt 
