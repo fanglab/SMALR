4dfd4568e226f061a864cb84940d8fcd3c68079a
John Beaulaurier <john.beaulaurier@gmail.com>
2018-04-09 10:16:43 -0700

    adding pysam >= 0.10.0 requirement and fixing README.rst typo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ __version__ = globals["__version__"]
 __author__ = globals["__author__"]
 __email__ = globals["__email__"]
 
-with open('README.md') as readme_file:
+with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 requirements = [
@@ -19,6 +19,8 @@ requirements = [
 	'h5py >= 2.0.1',
 	'numpy >= 1.7.1, < 1.14',
 	'cython >= 0.17',
+	'pysam >= 0.10.0',
+	'unittest2'
 	]
 
 setup_requirements = [
@@ -26,33 +28,37 @@ setup_requirements = [
 	"nose",
 ]
 
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
 setup(
-		name = 'smalr',
-		version=__version__,
-		author=__author__,
-		author_email=__email__,
-		description='A pipeline for single molecule-level modification analysis of SMRT reads',
-		long_description=readme,
-		url='https://github.com/jbeaulaurier/SMALR',
-		
-		packages=find_packages(include=['smalr']),
-		include_package_data=True,
-		package_data= {'smalr': ['R/*.r']},
-		zip_safe= False,
-		keywords='smalr methylation single-molecule bacteria SMRT pacbio',
+	name = 'smalr',
+	version=__version__,
+	author=__author__,
+	author_email=__email__,
+	description='A pipeline for single molecule-level modification analysis of SMRT reads',
+	long_description=readme,
+	url='https://github.com/jbeaulaurier/SMALR',
+	
+	packages=find_packages(include=['smalr']),
+	include_package_data=True,
+	package_data= {'smalr': ['R/*.r']},
+	zip_safe= False,
+	keywords='smalr methylation single-molecule bacteria SMRT pacbio',
 
-		entry_points={"console_scripts" : ["smalr = smalr.smalr_multicontig:main"]},
-		install_requires=requirements,
+	entry_points={"console_scripts" : ["smalr = smalr.smalr_multicontig:main"]},
+	install_requires=requirements,
 
-		classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: BSD License',
-		'Natural Language :: English',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-    	],
-    	test_suite='nose.collector',
-		tests_require=test_requirements,
-		setup_requires=setup_requirements,
+	classifiers=[
+	'Development Status :: 5 - Production/Stable',
+	'Intended Audience :: Developers',
+	'License :: OSI Approved :: BSD License',
+	'Natural Language :: English',
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 2.7',
+	],
+	test_suite='nose.collector',
+	tests_require=test_requirements,
+	setup_requires=setup_requirements,
 )
